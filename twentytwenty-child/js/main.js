a23fc486a1135347e55080a8664d13d05a887097
Sergio Vegas Alonso <sergio.vegas.alonso@gmail.com>
2021-01-18 23:23:05 +0100

    arrows ok

--- twentytwenty-child/js/main.js
+++ twentytwenty-child/js/main.js
@@ -11,4 +11,42 @@ window.onload = function() {
     backdrop.parentNode.removeChild(backdrop);
   }, firstAnimationMsDuration);
 
+  // Arrow animations
+  let inPageTop = true;
+  const portfolio = document.querySelector('.portfolio');
+  let arrow = document.querySelector('.arrow');
+  let arrowRigth = document.querySelector('.arrow > .right');
+  let arrowLeft = document.querySelector('.arrow > .left');
+
+  function addArrowToTopAnimation() {
+    arrowRigth.classList.remove('right-to-bottom');
+    arrowLeft.classList.remove('left-to-bottom');
+    arrowRigth.classList.add('right-to-top');
+    arrowLeft.classList.add('left-to-top');
+  }
+
+  function addArrowToBottomAnimation() {
+    arrowRigth.classList.remove('right-to-top');
+    arrowLeft.classList.remove('left-to-top');
+    arrowRigth.classList.add('right-to-bottom');
+    arrowLeft.classList.add('left-to-bottom');
+  }
+
+  arrow.addEventListener('click', function() {
+    if (inPageTop) {
+      window.scroll(0, portfolio.offsetHeight);
+    } else {
+      window.scroll(0, 0);
+    }
+  })
+
+  window.addEventListener('scroll', function(e) {
+    if (window.scrollY === 0) {
+      inPageTop = true;
+      addArrowToBottomAnimation();
+    } else {
+      inPageTop = false;
+      addArrowToTopAnimation();
+    }
+  }) 
 };
--- twentytwenty-child/singular.php
+++ twentytwenty-child/singular.php
@@ -52,35 +52,29 @@
   </div>
 	<div class="page third">
     <div class="logos-wrapper">
+      <a href="https://www.unilever.com/">
+        <img class="cover-photo" src="<?php get_site_url(); ?>/wp-content/uploads/2020/12/Logo-Unilever.png">
+      </a>
       <a href="https://www.eldiarioalerta.com/">
         <img class="cover-photo" src="<?php get_site_url(); ?>/wp-content/uploads/2020/12/Logo-Alerta.png">
-      </a>
-      <a href="https://www.alsea.net/">
-        <img class="cover-photo" src="<?php get_site_url(); ?>/wp-content/uploads/2020/12/logo-ALSEA.png">
-      </a>
-      <a href="https://ajuntament.barcelona.cat/es/">
-        <img class="cover-photo" src="<?php get_site_url(); ?>/wp-content/uploads/2020/12/Logo-Ayuntamiento-de-Barcelona.png">
-      </a> 
-      <a href="https://www.tgifridays.es/">
-        <img class="cover-photo" src="<?php get_site_url(); ?>/wp-content/uploads/2020/12/Logo-Fridays.png"> 
-      </a>
-      <a href="https://www.gehealthcare.com/">
-        <img class="cover-photo" src="<?php get_site_url(); ?>/wp-content/uploads/2020/12/logo-ALSEA.png">
-      </a>
-      <a href="https://www.ginos.es/">
-        <img class="cover-photo" src="<?php get_site_url(); ?>/wp-content/uploads/2020/12/Logo-Ginos.png">
-      </a>
-      <a href="https://www.global-exchange.com/">
-        <img class="cover-photo" src="<?php get_site_url(); ?>/wp-content/uploads/2020/12/Logo-Global-Exchange.png">
-      </a>
-      <a href="https://www.grupovips.com/">
-        <img class="cover-photo" src="<?php get_site_url(); ?>/wp-content/uploads/2020/12/Logo-Grupo-Vips.png">
       </a>
       <a href="https://ratiopharm.es/">
         <img class="cover-photo" src="<?php get_site_url(); ?>/wp-content/uploads/2020/12/Logo-Ratiopharm.png">
       </a>
+      <a href="https://www.gehealthcare.com/">
+        <img class="cover-photo" src="<?php get_site_url(); ?>/wp-content/uploads/2020/12/Logo-GE.png">
+      </a>
+      <a href="https://ajuntament.barcelona.cat/es/">
+        <img class="cover-photo" src="<?php get_site_url(); ?>/wp-content/uploads/2020/12/Logo-Ayuntamiento-de-Barcelona.png">
+      </a>
       <a href="https://www.ree.es/">
         <img class="cover-photo" src="<?php get_site_url(); ?>/wp-content/uploads/2020/12/Logo-Red-Electrica-de-Espana.png">
+      </a>
+      <a href="https://www.zebra.com/es/es.html">
+        <img class="cover-photo" src="<?php get_site_url(); ?>/wp-content/uploads/2020/12/Logo-Symbol.png">
+      </a>
+      <a href="https://www.zebra.com/es/es.html">
+        <img class="cover-photo" src="<?php get_site_url(); ?>/wp-content/uploads/2020/12/Logo-Zebra-anterior-Symbol.png">
       </a>
       <a href="https://www.renfe.com/">
         <img class="cover-photo" src="<?php get_site_url(); ?>/wp-content/uploads/2020/12/Logo-Renfe.png">
@@ -88,23 +82,29 @@
       <a href="https://www.repsol.com/">
         <img class="cover-photo" src="<?php get_site_url(); ?>/wp-content/uploads/2020/12/Logo-Repsol.png">
       </a>
+      <a href="https://www.wagamama.com/">
+        <img class="cover-photo" src="<?php get_site_url(); ?>/wp-content/uploads/2020/12/Logo-Wagamama.png">
+      </a>
+      <a href="https://www.tgifridays.es/">
+        <img class="cover-photo" src="<?php get_site_url(); ?>/wp-content/uploads/2020/12/Logo-Fridays.png"> 
+      </a>
       <a href="https://www.starbucks.es/">
         <img class="cover-photo" src="<?php get_site_url(); ?>/wp-content/uploads/2020/12/Logo-Starbucks.png">
       </a>
-      <a href="https://www.zebra.com/es/es.html">
-        <img class="cover-photo" src="<?php get_site_url(); ?>/wp-content/uploads/2020/12/Logo-Symbol.png">
-      </a>
-      <a href="https://www.unilever.com/">
-        <img class="cover-photo" src="<?php get_site_url(); ?>/wp-content/uploads/2020/12/Logo-Unilever.png">
+      <a href="https://www.ginos.es/">
+        <img class="cover-photo" src="<?php get_site_url(); ?>/wp-content/uploads/2020/12/Logo-Ginos.png">
       </a>
       <a href="https://www.vips.es/">
         <img class="cover-photo" src="<?php get_site_url(); ?>/wp-content/uploads/2020/12/Logo-VIPS.png">
       </a>
-      <a href="https://www.wagamama.com/">
-        <img class="cover-photo" src="<?php get_site_url(); ?>/wp-content/uploads/2020/12/Logo-Wagamama.png">
+      <a href="https://www.grupovips.com/">
+        <img class="cover-photo" src="<?php get_site_url(); ?>/wp-content/uploads/2020/12/Logo-Grupo-Vips.png">
       </a>
-      <a href="https://www.zebra.com/es/es.html">
-        <img class="cover-photo" src="<?php get_site_url(); ?>/wp-content/uploads/2020/12/Logo-Zebra-anterior-Symbol.png">
+      <a href="https://www.alsea.net/">
+        <img class="cover-photo" src="<?php get_site_url(); ?>/wp-content/uploads/2020/12/logo-ALSEA.png">
+      </a>           
+      <a href="https://www.global-exchange.com/">
+        <img class="cover-photo" src="<?php get_site_url(); ?>/wp-content/uploads/2020/12/Logo-Global-Exchange.png">
       </a>
     </div>
   </div>
@@ -112,3 +112,8 @@
   </div>
 	<div class="page fourth"></div>
 </main>
+<div class="arrow">
+  <div class="long"></div>
+  <div class="right"></div>
+  <div class="left"></div>
+</div>
